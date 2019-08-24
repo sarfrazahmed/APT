9d43c938d727b37171b066d0350e59643f691876
Ubuntu <ubuntu@ip-172-31-33-22.ap-south-1.compute.internal>
2019-08-24 15:50:02 +0000

    Fixed simulation

--- Simulation/candle_generator.py
+++ Simulation/candle_generator.py
@@ -103,7 +103,7 @@ def start(name, date, access_token, interval):
 if __name__ == '__main__':
     name = sys.argv[1]
     date = str(sys.argv[2])
-    access_token = sys.argv[4]
+    access_token = sys.argv[3]
     # name = 'LT'
     # date = '2019-08-08'
     interval = '5minute'
