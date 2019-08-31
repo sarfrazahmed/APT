6ff43a4d244fc02991bb800ed1d8982c646d6e54
Sarfraz <sarfraz.contact@gmail.com>
2019-09-01 02:55:40 +0530

    OMS structure implemented

--- Live_Trading/Order_Manager.py
+++ Live_Trading/Order_Manager.py
@@ -4,7 +4,7 @@ from kiteconnect import KiteConnect
 import configparser
 import os
 from kiteconnect import KiteTicker
-import time
+from datetime import datetime, timedelta
 
 # Authenticate
 config = configparser.ConfigParser()
@@ -22,30 +22,38 @@ access_token = df.iloc[0][0]
 kite = KiteConnect(api_key=api_key)
 kite.set_access_token(access_token)
 
-# Connect to kite connect
-kws = KiteTicker(api_key, access_token)
+# # Connect to kite connect
+# kws = KiteTicker(api_key, access_token)
+#
+# # Start webhook
+# def on_order_update(ws, data):
+#     print(data)
+#     data = pd.DataFrame(data)
+#     data.to_csv('order_info.csv')
+#
+# kws.connect()
 
-# Start webhook
-def on_order_update(ws, data):
-    print(data)
-    data = pd.DataFrame(data)
-    data.to_csv('order_info.csv')
+# Check if order is generated from Strategy
+# filename = 'D:/DevAPT/APT/Paper_Trading/access_token.csv'
+# old_time = os.stat(filename).st_mtime
+# while True:
+#     if os.stat(filename).st_mtime != old_time:
+#         print("File updated")
+#         old_time = os.stat(filename).st_mtime
+#         time.sleep(1)
 
-kws.connect()
-
-#check if order is generated from Strategy
-filename = 'D:/DevAPT/APT/Paper_Trading/access_token.csv'
-old_time = os.stat(filename).st_mtime
-while True:
-    if os.stat(filename).st_mtime != old_time:
-        print("File updated")
-        old_time = os.stat(filename).st_mtime
-        time.sleep(1)
-
-
-order_set = kite.orders()
+previous_data = pd.read_csv('filename')
+previous_orders = kite.orders()
 while True:
     if datetime.now().second % 10 == 0:
-        if order_set != kite.orders():
-            print("Punch order")
-
+        orders = kite.orders()
+        if previous_orders != orders:
+            orders.to_csv('filename')
+        if datetime.now().minute % 5 == 0 and datetime.now().second >= 10:
+            data = pd.read_csv('filename')
+            if len(data) != len(previous_data):
+                new_order = data - previous_data
+                kite.place_order(new_order)
+            elif data != previous_data:
+                modified_order = new_order
+                kite.modify_order(modified_order)
