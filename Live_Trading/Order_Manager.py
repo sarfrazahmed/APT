950f3ce2235831c67f3609b8b59ea9c68b0e7e3b
Sarfraz <sarfraz.contact@gmail.com>
2019-09-03 01:41:23 +0530

    Implemented part of Order Management

--- Live_Trading/Order_Manager.py
+++ Live_Trading/Order_Manager.py
@@ -3,7 +3,7 @@ import pandas as pd
 from kiteconnect import KiteConnect
 import configparser
 import os
-from kiteconnect import KiteTicker
+import time
 from datetime import datetime, timedelta
 
 # Authenticate
@@ -42,18 +42,52 @@ kite.set_access_token(access_token)
 #         old_time = os.stat(filename).st_mtime
 #         time.sleep(1)
 
-previous_data = pd.read_csv('filename')
-previous_orders = kite.orders()
 while True:
-    if datetime.now().second % 10 == 0:
-        orders = kite.orders()
-        if previous_orders != orders:
-            orders.to_csv('filename')
-        if datetime.now().minute % 5 == 0 and datetime.now().second >= 10:
-            data = pd.read_csv('filename')
-            if len(data) != len(previous_data):
-                new_order = data - previous_data
-                kite.place_order(new_order)
-            elif data != previous_data:
-                modified_order = new_order
-                kite.modify_order(modified_order)
+    previous_data = pd.read_csv('filename')
+    all_orders = kite.orders()
+    if len(data) != 0:
+        current_order = len(data)
+        if datetime.now().second % 10 == 0:
+            # get all orders
+            all_orders = kite.orders()
+            # filter for current order
+            order = all_orders['order_id']
+            if previous_order != order:
+                # punch new order if stoploss gets hit
+                if order['status'] == 'COMPLETE':
+                    if order['transaction_type'] == 'BUY':
+                        transaction_type = 'SELL'
+                        target = get_target(token)
+                        stoploss = data.at[current_order, 'day_high']
+                        order_id = kite.place_order(variety='BO', order_type='LIMIT', product='MIS', validity='DAY',
+                                                transaction_type='transaction_type', price=data.at[current_order, 'price'],
+                                                squareoff=data.at[current_order, 'target'], stoploss=data.at[current_order, 'stoploss'])
+                # change order status
+                if order['status'] == 'Executed':
+                    data.at[current_order, 'status'] = 'COMPLETE'
+                elif order['status'] == 'Cancelled':
+                    data.at[current_order, 'status'] = 'CANCELLED'
+                elif order['status'] == 'Placed':
+                    data.at[current_order, 'status'] = 'OPEN'
+                previous_order = order
+                data.to_csv('filename')
+
+            if datetime.now().minute % 5 == 0 and datetime.now().second >= 10:
+                data = pd.read_csv('filename')
+
+                # place new order
+                if data.at[current_order, 'modified'] == 0:
+                    order_id = kite.place_order(variety='BO', order_type='LIMIT', product='MIS', validity='DAY',
+                                                transaction_type='transaction_type', price=data.at[current_order, 'price'],
+                                                squareoff=data.at[current_order, 'target'], stoploss=data.at[current_order, 'stoploss'])
+                    data.at[current_order, order_id] = order_id
+
+                # cancel previous order and place new order if semi-target hits
+                if data.at[current_order, 'Semi-Target_Status'] == 'Hit' and order.at[
+                    current_order, 'Status'] == 'Placed':
+                    kite.cancel_order(order_id='order_id')
+                    order_id = kite.place_order(variety='BO', order_type='LIMIT', product='MIS', validity='DAY',
+                                                transaction_type='transaction_type', price=data.at[current_order, 'price'],
+                                                squareoff=data.at[current_order, 'target'], stoploss=data.at[current_order, 'stoploss'])
+                    data.at[current_order, order_id] = order_id
+    time.sleep(1)
