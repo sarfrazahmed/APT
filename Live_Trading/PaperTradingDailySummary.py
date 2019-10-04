1a1d39760b1314a1f690739133a9fa869fade52a
AnubhabNiel <52856067+AnubhabNiel@users.noreply.github.com>
2019-10-04 15:23:38 +0530

    Updated Strategy For Live

---
+++ Live_Trading/PaperTradingDailySummary.py
@@ -0,0 +1,158 @@
+## Import Libraries
+###############################################################
+import pandas as pd
+import numpy as np
+import sys
+import os
+import requests
+from datetime import datetime
+import smtplib
+import mimetypes
+from email.mime.multipart import MIMEMultipart
+from email import encoders
+from email.message import Message
+from email.mime.audio import MIMEAudio
+from email.mime.base import MIMEBase
+from email.mime.image import MIMEImage
+from email.mime.text import MIMEText
+import pdfkit
+
+## Function to send mail
+###############################################################
+def send_mail(mail_from,mail_to,subject,message,file_paths,password):
+    msg = MIMEMultipart()
+    msg["From"] = mail_from
+    msg["To"] = mail_to
+    msg["Subject"] = subject
+
+    # attach the body with the msg instance
+    msg.attach(MIMEText(message, 'plain'))
+
+    # open the file to be sent
+    for file_path in file_paths:
+        attachment = open(file_path, "rb")
+        
+        # instance of MIMEBase and named as p
+        p = MIMEBase('application', 'octet-stream')
+
+        # To change the payload into encoded form
+        p.set_payload((attachment).read())
+
+        # encode into base64
+        encoders.encode_base64(p)
+
+        p.add_header('Content-Disposition', "attachment; filename= %s" % file_path)
+
+        # attach the instance 'p' to instance 'msg'
+        msg.attach(p)
+
+    # creates SMTP session
+    s = smtplib.SMTP('smtp.gmail.com', 587)
+
+    # start TLS for security
+    s.starttls()
+
+    # Authentication
+    s.login(mail_from, password)
+
+    # Converts the Multipart msg into a string
+    text = msg.as_string()
+
+    # sending the mail
+    s.sendmail(mail_from, mail_to, text)
+
+    # terminating the session
+    s.quit()
+
+
+## Initial Inputs
+###############################################################
+# Windows
+# folder_path = 'F:/DevAPT/APT/Paper_Trading'
+# wkhtmltopdf_path = 'F:/DevAPT/APT/Paper_Trading/wkhtmltopdf/bin/wkhtmltopdf.exe'
+
+# For Ubuntu
+folder_path = '/home/ubuntu/APT/APT/Paper_Trading'
+wkhtmltopdf_path = '/home/ubuntu/anaconda3/bin/wkhtmltopdf'
+
+file_phrase = 'PaperTrading_Output'
+stock_list_path = 'stock_list_updated.csv'
+
+time_now = str(datetime.now().date())
+body_text = 'Hi All,\n\nPFA the summary of paper trading for ' + time_now + '\n\nRegards,\nAPT BOT'
+bot_mail_id = 'apt.automated@gmail.com'
+bot_mail_password = 'algotrading2019'
+recipients = ['anubhab.ghosh95@gmail.com','sarfraz.contact@gmail.com','arkajeet75@gmail.com']
+
+## Main Body
+###############################################################
+stock_list = pd.read_csv(folder_path + '/' + stock_list_path)
+print('Stock List Imported',flush=True)
+
+os.chdir(folder_path)
+summary_df = pd.DataFrame()
+for i in stock_list.index.values:
+    # Get the Stock wise Output File names
+    stock_name = stock_list['Company'][i]
+    file = file_phrase + stock_name + '.csv'
+
+    trade_data = pd.read_csv(file)
+    print('Calculating For ' + stock_name,flush=True)
+    trade_count = trade_data['Order_Status'][trade_data['Order_Status'] == 'Exit'].count()
+
+    trade_data['Double_Entry_Flag'] = np.where(trade_data['Order_Status'] == 'Exit',
+                                               np.where(trade_data['Target'] != 0.0,1,0),0)
+    trade_data['Amount'] = np.where(trade_data['Order_Signal'] == 'Buy',
+                                    np.where(trade_data['Double_Entry_Flag'] == 1,
+                                             (-2) * trade_data['Order_Price'],
+                                             (-1) * trade_data['Order_Price']),
+                                    np.where(trade_data['Order_Signal'] == 'Sell',
+                                             np.where(trade_data['Double_Entry_Flag'] == 1,
+                                                      2 * trade_data['Order_Price'],
+                                                      trade_data['Order_Price']),0.0))
+    profit = trade_data['Amount'].sum()
+    net_profit = (profit * stock_list['Lot_Size'][i]) - (trade_count * 200)
+    summary_df = pd.concat([summary_df,pd.DataFrame(data = [[stock_name,profit,trade_count,net_profit]],
+                                                    columns = ['Company','Profit','Trades','Net_Profit'])])
+    print('Completed',flush=True)
+
+# Calculate Total Profit
+total_profit = summary_df.Net_Profit.sum()
+total_trades = summary_df.Trades.sum()
+total_summary = pd.DataFrame([['Total',np.nan,total_trades,total_profit]],
+                             columns = ['Company','Profit','Trades','Net_Profit'])
+summary_df = pd.concat([summary_df,total_summary])
+
+
+# write Summary as CSV
+print('Writing Summary as CSV',flush=True)
+time_now = str(datetime.now().date())
+output_file_path = 'Paper_Trading_Summary_' + time_now +'.csv'
+summary_df.to_csv(output_file_path,index= False)
+
+# Send csv as Mail
+print('Sending CSV as Email',flush=True)
+attachments = [file_phrase + stock_name + '.csv' for stock_name in stock_list['Company']]
+print(attachments, flush = True)
+print(output_file_path, flush = True)
+attachments.insert(0, output_file_path)
+print(attachments)
+
+for target_mail_id in recipients:
+    send_mail(bot_mail_id, target_mail_id, 'Paper Trading Result of The Day',
+              body_text, attachments, bot_mail_password)
+
+# Save csv as PDF
+output_pdf_path = output_file_path[:(len(output_file_path) - 3)] + 'pdf'
+output_html_path = output_file_path[:(len(output_file_path) - 3)] + 'html'
+summary_df.to_html(output_html_path,index=False)
+config = pdfkit.configuration(wkhtmltopdf= wkhtmltopdf_path)
+pdfkit.from_file(output_html_path,output_pdf_path,configuration=config)
+
+# Send pdf to telegram
+pdf_summary = open(output_pdf_path, 'rb')
+requests.post("https://api.telegram.org/bot823468101:AAEqDCOXI3zBxxURkTgtleUvFvQ0S9a4TXA/sendDocument?chat_id=-383311990",
+              files={'document': pdf_summary})
+message = 'csv file of daily report is sent in email'
+requests.get("https://api.telegram.org/bot823468101:AAEqDCOXI3zBxxURkTgtleUvFvQ0S9a4TXA/sendMessage?chat_id=-383311990&text=" + message)
+
---
+++ Live_Trading/authenticator.py
@@ -0,0 +1,143 @@
+# Import dependencies
+import configparser
+import re
+import os
+from selenium import webdriver
+import time
+from datetime import datetime
+print('Start Time: ' + str(datetime.now()))
+from kiteconnect import KiteConnect
+import pandas as pd
+
+# Get all info
+print("Starting Trading Engine...", flush=True)
+config = configparser.ConfigParser()
+
+
+## Initial Inputs
+############################################################################
+# For Windows
+# path = 'F:/DevAPT/APT/Paper_Trading'
+
+# For Ubuntu
+path = '/home/ubuntu/APT/APT/Paper_Trading'
+
+# For Windows
+# path = 'D:/DevAPT/APT/Paper_Trading'
+
+exception_stocks = ['IDEA','BOSCHLTD','CASTROLIND','IDBI','MRF','NBCC','DISHTV',
+                    'GMRINFRA','RELINFRA']
+info_data_path = 'nifty50_stocks_token.csv'
+nsepage = 'https://www.nseindia.com/live_market/dynaContent/live_watch/pre_open_market/pre_open_market.htm'
+kitepage = 'https://kite.zerodha.com/'
+
+
+
+## Main Body
+################################################################
+os.chdir(path)
+info_data = pd.read_csv(info_data_path)
+config_path = path + '/config.ini'
+config.read(config_path)
+api_key = config['API']['API_KEY']
+api_secret = config['API']['API_SECRET']
+username = config['USER']['USERNAME']
+password = config['USER']['PASSWORD']
+pin = config['USER']['PIN']
+
+
+## Selenium for ubuntu
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+driver = webdriver.Chrome(chrome_options=chrome_options)
+page = driver.get(nsepage)
+time.sleep(5)
+
+# Selenium for windows
+# driver = webdriver.Chrome(executable_path='F:\\DevAPT\\APT\\chromedriver.exe')
+# page = driver.get(nsepage)
+# time.sleep(5)
+
+# Choose All FO stocks From Option
+print('Getting All Gap Up/ Down Stocks From Pre Market...')
+x = driver.find_element_by_xpath('//*[@id="selId"]/option[2]')
+x.click()
+time.sleep(3)
+
+# Get Pre-market data as DataFrame
+table = driver.find_element_by_xpath('//*[@id="preOpenNiftyTab"]')
+print(table,flush= True)
+tbl = table.get_attribute('outerHTML')
+pre_open_stat = pd.read_html(tbl)[0]
+pre_open_stat = pre_open_stat.drop([1,2,3,4,6,7,8,9,10,11],axis=1)
+pre_open_stat.columns = pre_open_stat.iloc[1]
+pre_open_stat = pre_open_stat[2:]
+pre_open_stat['% Chng'] = [float(i) for i in pre_open_stat['% Chng']]
+
+
+# Select Stocks From Pre-Open Scenario
+pre_open_stat['Abs_Change'] = abs(pre_open_stat['% Chng'])
+pre_open_stat = pre_open_stat[~pre_open_stat.Symbol.isin(exception_stocks)]
+
+# Scenario 1
+selected_scrips = pre_open_stat[pre_open_stat['Abs_Change'] >= 0.9]
+selected_scrips = selected_scrips.sort_values(['Abs_Change'],ascending=False)
+selected_scrips = selected_scrips[['Symbol', '% Chng', 'Abs_Change']]
+
+# Include Token & Lot_Size
+selected_scrips_info = pd.merge(selected_scrips,info_data,
+                                left_on='Symbol',
+                                right_on='Company',
+                                how='left')
+selected_scrips_info = selected_scrips_info.dropna()
+selected_scrips_info = selected_scrips_info.reset_index()
+selected_scrips_info = selected_scrips_info[0:5]
+selected_scrips_info = selected_scrips_info[['Company','Token','Lot_Size']]
+print('Completed')
+
+
+
+# Login using username and password
+print("Authenticating For Generation of Access Token...", flush=True)
+page = driver.get(kitepage)
+time.sleep(3)
+
+# Logging in using Username and Password
+user_id_box = driver.find_element_by_xpath(
+    '//*[@id="container"]/div/div/div/form/div[2]/input')
+password_box = driver.find_element_by_xpath(
+    '//*[@id="container"]/div/div/div/form/div[3]/input')
+log_in_button = driver.find_element_by_xpath(
+    '//*[@id="container"]/div/div/div/form/div[4]/button')
+user_id_box.send_keys(username)
+password_box.send_keys(password)
+log_in_button.click()
+time.sleep(3)
+
+# Log in using Pin
+pin_box = driver.find_element_by_xpath(
+    '//*[@id="container"]/div/div/div/form/div[2]/div/input')
+continue_box = driver.find_element_by_xpath(
+    '//*[@id="container"]/div/div/div/form/div[3]/button')
+pin_box.send_keys(pin)
+continue_box.click()
+time.sleep(3)
+
+# Redirecting to Kiteconnect
+kite = KiteConnect(api_key=api_key)
+url = kite.login_url()
+page = driver.get(url)
+current_url = driver.current_url
+request_token = re.search('request_token=(.*)', current_url).group(1)[:32]
+KRT = kite.generate_session(request_token, api_secret)
+print("Connection Successful")
+driver.close()
+
+# Write access token
+selected_scrips_info['Access_Token'] = KRT['access_token']
+selected_scrips_info['Extra'] = 0
+selected_scrips_info.to_csv('stock_list_updated.csv', index=False)
+print("Connection successful", flush=True)
+print("End Time: " + str(datetime.now()))
