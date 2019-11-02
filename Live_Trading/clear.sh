d407dea53626ccc832a3c09d907f91e29b0eb11a
Ubuntu <ubuntu@ip-172-31-33-22.ap-south-1.compute.internal>
2019-11-02 13:42:20 +0000

    Daily files

--- Live_Trading/clear.sh
+++ Live_Trading/clear.sh
@@ -1,3 +1,4 @@
 kill -9 $(ps ax | grep google | fgrep -v grep | awk '{ print $1 }')
 kill -9 $(ps ax | grep chrome | fgrep -v grep | awk '{ print $1 }')
 kill -9 $(ps ax | grep python | fgrep -v grep | awk '{ print $1 }')
+kill -9 $(ps ax | grep sh | fgrep -v grep | awk '{ print $1 }')
