813cf7f07af3722e5a7cf1e630d4ab9f2b022d79
AnubhabNiel <anubhab.juee@gmail.com>
2019-11-12 16:15:25 -0800

    Testing in Anubhab

---
+++ Simulation/Test_Socket_Server.py
@@ -0,0 +1,22 @@
+import socket
+import time
+import pickle
+
+
+HEADERSIZE = 10
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((socket.gethostname(), 1243))
+print('Host Name: ', socket.gethostname())
+s.listen(5)
+
+while True:
+    # now our endpoint knows about the OTHER endpoint.
+    clientsocket, address = s.accept()
+    print(f"Connection from {address} has been established.")
+
+    d = {1:"hi", 2: "there"}
+    msg = pickle.dumps(d)
+    msg = bytes(f"{len(msg):<{HEADERSIZE}}", 'utf-8')+msg
+    print(msg)
+    clientsocket.send(msg)
